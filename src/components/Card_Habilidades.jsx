effa78152a542b25badf71628306df21f3b8e31e
Nestor20MartinezHouse <nestor20martinez@gmail.com>
2025-03-24 16:23:01 -0500

    agregando estilos a las card de habilidades

--- src/components/Card_Habilidades.jsx
+++ src/components/Card_Habilidades.jsx
@@ -1,15 +1,15 @@
-export default function Card_Habilidades({ url, nombreH }) {
+export default function Card_Habilidades({ urlH, nombreH }) {
   return (
     <>
       <div className="cad_habilidades">
         <img
-          src={url}
+          src={urlH}
           alt="HTML"
           loading="lazy"
           decoding="async"
           className="img_habilidades"
         />
-        <p>{nombreH}</p>
+        <p className="titulo_Habilidaes">{nombreH}</p>
       </div>
     </>
   );
--- src/components/Habilidades.jsx
+++ src/components/Habilidades.jsx
@@ -5,62 +5,66 @@ export default function Habilidades() {
     <section className="container_habilidades">
       <Card_Habilidades
         nombreH={"HTML"}
-        url={"https://www.vectorlogo.zone/logos/w3_html5/w3_html5-icon.svg"}
+        urlH={"https://www.vectorlogo.zone/logos/w3_html5/w3_html5-icon.svg"}
       />
 
       <Card_Habilidades
         nombreH={"CSS"}
-        url={"https://www.vectorlogo.zone/logos/w3_css/w3_css-icon~old.svg"}
+        urlH={"https://www.vectorlogo.zone/logos/w3_css/w3_css-icon~old.svg"}
       />
       <Card_Habilidades
         nombreH={"JavaScript"}
-        url={"https://www.vectorlogo.zone/logos/javascript/javascript-icon.svg"}
+        urlH={
+          "https://www.vectorlogo.zone/logos/javascript/javascript-icon.svg"
+        }
       />
       <Card_Habilidades
         nombreH={"Astro"}
-        url={"https://svgl.app/library/astro.svg"}
+        urlH={"https://svgl.app/library/astro.svg"}
       />
       <Card_Habilidades
         nombreH={"React"}
-        url={"https://www.vectorlogo.zone/logos/reactjs/reactjs-icon.svg"}
+        urlH={"https://www.vectorlogo.zone/logos/reactjs/reactjs-icon.svg"}
       />
       <Card_Habilidades
         nombreH={"PHP"}
-        url={"https://www.vectorlogo.zone/logos/php/php-icon.svg"}
+        urlH={"https://www.vectorlogo.zone/logos/php/php-icon.svg"}
       />
       <Card_Habilidades
         nombreH={"laravel"}
-        url={"https://www.vectorlogo.zone/logos/laravel/laravel-icon.svg"}
+        urlH={"https://www.vectorlogo.zone/logos/laravel/laravel-icon.svg"}
       />
       <Card_Habilidades
         nombreH={"Boostrap"}
-        url={
+        urlH={
           "https://www.vectorlogo.zone/logos/getbootstrap/getbootstrap-icon.svg"
         }
       />
       <Card_Habilidades
         nombreH={"Tailwind"}
-        url={
+        urlH={
           "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg"
         }
       />
       <Card_Habilidades
         nombreH={"Mysql"}
-        url={"https://www.vectorlogo.zone/logos/mysql/mysql-icon.svg"}
+        urlH={"https://www.vectorlogo.zone/logos/mysql/mysql-icon.svg"}
       />
       <Card_Habilidades
         nombreH={"TypeScript"}
-        url={
+        urlH={
           "https://www.vectorlogo.zone/logos/typescriptlang/typescriptlang-icon.svg"
         }
       />
       <Card_Habilidades
         nombreH={"Git"}
-        url={"https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg"}
+        urlH={"https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg"}
       />
       <Card_Habilidades
         nombreH={"Postman"}
-        url={"https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg"}
+        urlH={
+          "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg"
+        }
       />
     </section>
   );
